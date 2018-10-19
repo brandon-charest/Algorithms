0e5ae18259769cff4c3652c6a867ad08e6549e78
Brandon <b.charest019@gmail.com>
2018-10-19 13:19:42 -0400

    BFS

--- Algorithms/BFS/BFS.cpp
+++ Algorithms/BFS/BFS.cpp
@@ -28,26 +28,23 @@ void BFS(Graph const graph, int vertex)
 
 }
 
-
-
 int main()
 {
 
-	Graph graph(15);
+	Graph graph(10);
 	graph.isDirected = false;
-	graph.addEdge(1, 2);
-	graph.addEdge(1, 3);
-	graph.addEdge(1, 4);
+	graph.addEdge(0, 1);
+	graph.addEdge(0, 2);
+	graph.addEdge(0, 3);
+	graph.addEdge(0, 4);
+	graph.addEdge(1, 5);
 	graph.addEdge(2, 5);
-	graph.addEdge(2, 6);
-	graph.addEdge(5, 9);
-	graph.addEdge(5, 10);
-	graph.addEdge(4, 7);
-	graph.addEdge(4, 8);
-	graph.addEdge(7, 11);
-	graph.addEdge(7, 12);
+	graph.addEdge(3, 6);
+	graph.addEdge(4, 6);
+	graph.addEdge(5, 7);
+	graph.addEdge(6, 7);
 
-	BFS(graph, 7);
+	BFS(graph, 0);
 
 	return 0;
 }
